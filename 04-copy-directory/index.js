e3c8e0c9c069b02d68e13f51f9b2bd740a3cb577
Borys Maksymov <radiatorzp@gmail.com>
2022-11-05 09:39:57 +0200

    feat: forth task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const pathSrc = path.join(__dirname, 'files');
+const pathDst = path.join(__dirname, 'files-copy');
 
+if (fs.existsSync(pathDst)) {
+    console.log('Destination folder already exists!')
+} else {
+    fs.mkdir(pathDst, (err) => {
+        if (err) console.log(err);
+    });    
+}
+
+fs.readdir(pathSrc, { withFileTypes: true }, (err, files) => {
+    if (err) {
+        console.log(err);
+    } else {
+        files.forEach(file => {
+            let fileSrc = path.join(pathSrc, file.name);
+            let fileDst = path.join(pathDst, file.name);
+            fs.copyFile(fileSrc, fileDst, (err) => {
+                if (err) console.log(err);
+                console.log(`Copied ${file.name}` + '\t' + 'to folder ' + pathDst);
+            });
+        });    
+    }
+})
