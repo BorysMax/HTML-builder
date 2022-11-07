b460e229c642c54ab625caecefd1e3d90e536922
Borys Maksymov <radiatorzp@gmail.com>
2022-11-07 14:25:57 +0200

    feat: sixth task done

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,26 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const pathSrc = path.join(__dirname, 'styles');
+const pathHtml = path.join(__dirname, 'components');
 const pathDst = path.join(__dirname, 'project-dist');
 const pathAssets = path.join(__dirname, 'project-dist', 'assets');
-const fileBundle = path.join(__dirname, 'project-dist', 'style.css');
-
-function makeDir (pathDir) {
-  fs.exists(pathDir, (exists) => {
-    if (exists) {
-        console.log(`${pathDir}  folder already exists!`)
-    } else {
-        fs.mkdir(pathDir, (err) => {
-            if (err) console.log(err);
-        });    
-    }
-  });
-}
+const fileBundleCss = path.join(__dirname, 'project-dist', 'style.css');
+const fileBundleHtml = path.join(__dirname, 'project-dist', 'index.html');
 
 makeDir(pathDst);
 makeDir(pathAssets);
+copyFilesFromDir (path.join(__dirname, 'assets'), path.join(pathDst, 'assets'));
 
-fs.open(fileBundle, 'w', (err) => {
+fs.open(fileBundleCss, 'w', (err) => {
   if(err) console.log(err);
 });
 
@@ -36,7 +27,7 @@ fs.readdir(pathSrc, { withFileTypes: true }, (err, files) => {
           if(err) {s
             console.log(err); 
           } else {
-            fs.appendFile(fileBundle, data, (err) => {
+            fs.appendFile(fileBundleCss, data, (err) => {
               if(err) console.log(err); 
             });    
           }  
@@ -46,7 +37,62 @@ fs.readdir(pathSrc, { withFileTypes: true }, (err, files) => {
   }
 })
 
-function copyFileFromDir (pathSrc, pathDst) {
+let filename = path.join(__dirname, 'template.html');
+let array = [];
+let resArray = [];
+
+function addArray (data, spaceNum) {
+  let array = data.toString().split("\n").map(item => item = " ".repeat(spaceNum) + item); 
+  return array;
+}
+
+function writeToFile (inputArr) {
+  let content = inputArr.join("\n");
+  fs.writeFile(path.join(pathDst, "index.html"), content, (err) => {
+    if(err) console.log(err); 
+  });    
+}
+
+const main = async () => {
+  let dataTemp = await fs.promises.readFile(filename, 'utf8');
+  let dataHeader = await fs.promises.readFile(path.join(pathHtml, 'header.html'), 'utf8');
+  let dataFooter = await fs.promises.readFile(path.join(pathHtml, 'footer.html'), 'utf8');
+  let dataArt = await fs.promises.readFile(path.join(pathHtml, 'articles.html'), 'utf8');
+  array = addArray (dataTemp, 0);
+  for(i in array) {
+    resArray.push(array[i]);
+    if (array[i].trim() === '{{header}}') {
+      console.log('Header!');
+      resArray.pop(array[i]);
+      resArray = resArray.concat(addArray(dataHeader, 4));
+    } else if (array[i].trim() === '{{footer}}') {
+      console.log('Footer!');
+      resArray.pop(array[i]);
+      resArray = resArray.concat(addArray(dataFooter, 4));
+    } else if (array[i].trim() === '{{articles}}') {
+      console.log('Articles!');
+      resArray.pop(array[i]);
+      resArray = resArray.concat(addArray(dataArt, 6));
+    }
+  }  
+  writeToFile (resArray);
+};
+  
+main().catch(console.error);
+
+function makeDir (pathDir) {
+  fs.exists(pathDir, (exists) => {
+    if (exists) {
+        console.log(`${pathDir}  folder already exists!`)
+    } else {
+        fs.mkdir(pathDir, (err) => {
+            if (err) console.log(err);
+        });    
+    }
+  });
+}
+
+function copyFilesFromDir (pathSrc, pathDst) {
   fs.readdir(pathSrc, { withFileTypes: true }, (err, files) => {
     if (err) {
         console.log(err);
@@ -57,7 +103,7 @@ function copyFileFromDir (pathSrc, pathDst) {
           if (file.isDirectory()) {
             makeDir(fileDst);
             console.log(`Copied Folder ${file.name}` + '\t' + 'to folder ' + pathDst);
-            copyFileFromDir (path.join(pathSrc, file.name), path.join(pathDst, file.name));
+            copyFilesFromDir (path.join(pathSrc, file.name), path.join(pathDst, file.name));
           } else {
             fs.copyFile(fileSrc, fileDst, (err) => {
               if (err) console.log(err);
@@ -68,6 +114,3 @@ function copyFileFromDir (pathSrc, pathDst) {
     }
   })
 }
-
-copyFileFromDir (path.join(__dirname, 'assets'), path.join(pathDst, 'assets'));
-
